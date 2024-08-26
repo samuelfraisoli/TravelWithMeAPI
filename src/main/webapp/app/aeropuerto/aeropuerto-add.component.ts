e3c64bf3e05ac0fefc86e258f107594d33d0b9e0
Samuel Fraisoli <samuelfraisoli@gmail.com>
2024-08-26 16:44:50 +0200

    add aeropuerto editado

--- src/main/webapp/app/aeropuerto/aeropuerto-add.component.ts
+++ src/main/webapp/app/aeropuerto/aeropuerto-add.component.ts
@@ -16,26 +16,70 @@ import { ErrorHandler } from 'app/common/error-handler.injectable';
 })
 export class AeropuertoAddComponent {
 
+
   aeropuertoService = inject(AeropuertoService);
   router = inject(Router);
   errorHandler = inject(ErrorHandler);
 
-  addForm = new FormGroup({
-    nombre: new FormControl(null, [Validators.required, Validators.maxLength(255)]),
-    ciudad: new FormControl(null, [Validators.required, Validators.maxLength(255)]),
-    ciudadAbrev: new FormControl(null, [Validators.required, Validators.maxLength(255)]),
-    pais: new FormControl(null, [Validators.required, Validators.maxLength(255)])
-  }, { updateOn: 'submit' });
+  addForm: FormGroup;
 
-  getMessage(key: string, details?: any) {
-    const messages: Record<string, string> = {
-      created: $localize`:@@aeropuerto.create.success:Aeropuerto creado correctamente.`
-    };
-    return messages[key];
+  constructor() {
+    this.addForm = new FormGroup({
+      nombre: new FormControl(null, [Validators.required, Validators.maxLength(255)]),
+      ciudad: new FormControl(null, [Validators.required, Validators.maxLength(255)]),
+      ciudadAbrev: new FormControl(null, [Validators.required, Validators.maxLength(255)]),
+      pais: new FormControl(null, [Validators.required, Validators.maxLength(255)])
+    }, { updateOn: 'submit' });
+
+  }
+
+  //formgroup
+  
+
+  //mensajes de error para todas las propiedades
+  errorMessages: any = {
+    nombre: {
+      required: 'Nombre es requerido',
+      maxlength: 'Nombre no puede tener más de 255 caracteres'
+    },
+    ciudad: {
+      required: 'Ciudad es requerida',
+      maxlength: 'Ciudad no puede tener más de 255 caracteres'
+    },
+    ciudadAbrev: {
+      required: 'Abreviatura es requerida',
+      maxlength: 'Abreviatura no puede tener más de 255 caracteres'
+    },
+    pais: {
+      required: 'Pais es requerido',
+      maxlength: 'Pais no puede tener más de 255 caracteres',
+      positiveNumber: 'El número debe ser positivo'
+    }
+  };
+
+  //
+  getMessage() {
+    return "Aeropuerto creado correctamente"
+  }
+
+  getError(controlName: string): string {
+    const control = this.addForm.get(controlName);
+    if (control && control.invalid && (control.dirty || control.touched)) {
+      const errors = control.errors;
+      //si errors existe, o sea si el formcontrol tiene errores, va a buscar en el array los strings
+      if (errors) {
+        console.log("errors" + errors + errors[0])
+        const errorKey = Object.keys(errors)[0];
+        return this.errorMessages[controlName][errorKey];
+       
+      }
+    }
+    return '';
   }
 
   handleSubmit() {
     window.scrollTo(0, 0);
+    //marca todos los campos como touched para que se revisen todos y salgan mensajes de error si los hay
     this.addForm.markAllAsTouched();
     if (!this.addForm.valid) {
       return;
@@ -44,11 +88,12 @@ export class AeropuertoAddComponent {
     this.aeropuertoService.createAeropuerto(data)
         .subscribe({
           next: () => this.router.navigate(['/aeropuertos'], {
+            //state envía al router de angular que el estado del formulario es correcto
             state: {
-              msgSuccess: this.getMessage('created')
+              msgSuccess: this.getMessage()
             }
           }),
-          error: (error) => this.errorHandler.handleServerError(error.error, this.addForm, this.getMessage)
+          error: (error) => this.errorHandler.handleServerError(error.error, this.addForm)
         });
   }
 
